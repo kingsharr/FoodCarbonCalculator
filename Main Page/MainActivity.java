ef697fa7b82edef6437e3d5ca00e3c57488713db
ADAMMUQRI <116181167+ADAMMUQRI@users.noreply.github.com>
2024-01-04 14:59:29 +0800

    Update MainActivity.java

--- Main Page/MainActivity.java
+++ Main Page/MainActivity.java
@@ -1,1 +1,86 @@
 
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.navigation.Navigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView BTNdashboard;
+    CardView cardShopping, cardCarbon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.bottom_home) {
+                // Handle home item selection
+                return true;
+            } else if (id == R.id.bottom_calendar) {
+                startActivity(new Intent(getApplicationContext(), MealPlannerActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (id == R.id.bottom_calculator) {
+                startActivity(new Intent(getApplicationContext(), FoodCalculatorActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (id == R.id.bottom_comment) {
+                startActivity(new Intent(getApplicationContext(), CommentActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (id == R.id.bottom_profile) {
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else {
+                return false;
+            }
+
+        });
+
+        BTNdashboard = findViewById(R.id.dashboardImage);
+        BTNdashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(com.example.foodcarboncalculator.MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardShopping = findViewById(R.id.button_discover1);
+        cardShopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShoppingListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardCarbon = findViewById(R.id.button_discover2);
+        cardCarbon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CarbonTipsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
