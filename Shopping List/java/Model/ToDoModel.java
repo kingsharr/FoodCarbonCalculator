412a731afb1a1a6efc6d501899f6438b37db43de
Husna Ihsanuddin <121876818+husnaihsan@users.noreply.github.com>
2024-01-05 14:08:37 +0800

    Add files via upload

---
+++ Shopping List/java/Model/ToDoModel.java
@@ -0,0 +1,19 @@
+package com.example.foodcarboncalculator.Model;
+
+public class ToDoModel extends TaskId {
+
+    private String task , note;
+    private int status;
+
+    public String getTask() {
+        return task;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
