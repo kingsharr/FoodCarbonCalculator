a3369cc510ebf18edd3848fd2113e9d9195911a4
Husna Ihsanuddin <121876818+husnaihsan@users.noreply.github.com>
2024-01-07 02:09:24 +0800

    Update EditProfile.java

--- user accessibility/EditProfile.java
+++ user accessibility/EditProfile.java
@@ -1,25 +1,32 @@
 package com.example.foodcarboncalculator;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,14 +35,16 @@ public class EditProfile extends AppCompatActivity {
     EditText editName, editEmail, editAge, editCarbon;
     Spinner spinnerGender, spinnerDiet;
     Button saveButton, logoutButton, cancelButton;
-    CircleImageView imageView;
+    CircleImageView ProfileImage;
     Long ageUser;
     Double  carbonUser;
     String nameUser, emailUser, usernameUser, dietUser, genderUser;
     DocumentReference reference;
+    FirebaseFirestore fStore;
     FirebaseAuth auth;
     FirebaseDatabase database;
     FirebaseStorage storage;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +54,13 @@ public class EditProfile extends AppCompatActivity {
         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         reference = db.collection("users").document(userId);
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         database = FirebaseDatabase.getInstance();
         storage = FirebaseStorage.getInstance();
         auth = FirebaseAuth.getInstance();
 
-        imageView = findViewById(R.id.imageView2);
+        ProfileImage = findViewById(R.id.imageView2);
 
         editName = findViewById(R.id.editName);
         editEmail = findViewById(R.id.editEmail);
@@ -63,6 +73,14 @@ public class EditProfile extends AppCompatActivity {
 
         showData();
 
+        StorageReference profileRef = storageReference.child("users/"+auth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(ProfileImage);
+            }
+        });
+
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +92,14 @@ public class EditProfile extends AppCompatActivity {
                 } else {
                     Toast.makeText(EditProfile.this, "No Changes Found", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        ProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent,1000);
             }
         });
 
@@ -236,6 +262,15 @@ public class EditProfile extends AppCompatActivity {
             return false;
         }
     }
+    private boolean isProfileChanged(Uri imageUri) {
+        if (imageUri != null) {
+            // If a new image is selected, it means the profile picture is changed
+            return true;
+        } else {
+            // If the imageUri is null, it means the profile picture remains unchanged
+            return false;
+        }
+    }
 
 
     public void showData() {
@@ -294,6 +329,44 @@ public class EditProfile extends AppCompatActivity {
 
                 // Set the default selection to "Preference"
                 spinner.setSelection(0);}
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+
+                //profileImage.setImageURI(imageUri);
+
+                uploadImageToFirebase(imageUri);
+
+
+            }
+        }
+
+    }
+    private void uploadImageToFirebase(Uri imageUri) {
+        // uplaod image to firebase storage
+        final StorageReference fileRef = storageReference.child("users/"+auth.getCurrentUser().getUid()+"/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(ProfileImage);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
